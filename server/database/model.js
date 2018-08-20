879daf078081097210bfc4ba7a4308c988f3b4bf
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-08-20 15:19:33 -0700

    Change to Connect to Postgres Database on ec2 instance

--- server/database/model.js
+++ server/database/model.js
@@ -1,10 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const config = {
+  serverName: process.env.DB_HOST,
   user: process.env.DB_user,
-  database: process.env.DB_database,
-  // port: ####,
+  databaseName: process.env.DB_database,
+  portNumber: 5432,
+  password: process.env.DB_PASSWORD,
   max_connections: 50,
 };
 
@@ -12,6 +14,7 @@ const pool = new Pool(config);
 
 pool.connect((err, poolClient) => {
   if (err) {
+    console.log('test');
     return console.error('Error acquiring client', err.stack);
   }
   console.log('client has connected');
