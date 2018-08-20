9ee1d504399c7e249e0903ed2de77787604e3403
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-08-20 01:56:30 -0700

    Edit Siege Attack

--- benchmark.js
+++ benchmark.js
@@ -2,7 +2,7 @@ const siege = require('siege');
 
 const stresser = siege().on(3004);
 
-for (let i = 0; i < 1000; i += 1) {
+for (let i = 0; i < 30000; i += 1) {
   // for (let i = 9000000; i < 9090000; i += 1) {
   const listingId = 9000000 + Math.floor(Math.random() * 999999);
   batteryRam = stresser.for(1).times.get(`/api/listings/${listingId}`);
