f13fe4e9dc5073c210ecf47980fc7997a704417d
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-08-19 18:27:31 -0700

    Edit Siege to request Random Listings Ids

--- benchmark.js
+++ benchmark.js
@@ -2,12 +2,10 @@ const siege = require('siege');
 
 const stresser = siege().on(3004);
 
-// for (let i = 0; i < 1000; i += 1) {
-  // const listingId = 9000000 + Math.floor(Math.random() * 999999);
-
-
-for (let i = 9000000; i < 9090000; i += 1) {
-  batteryRam = stresser.for(1).times.get(`/api/listings/${i}`);
+for (let i = 0; i < 1000; i += 1) {
+  // for (let i = 9000000; i < 9090000; i += 1) {
+  const listingId = 9000000 + Math.floor(Math.random() * 999999);
+  batteryRam = stresser.for(1).times.get(`/api/listings/${listingId}`);
 }
 
 batteryRam.attack();
